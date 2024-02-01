57cd6f949cc0bba21525de09a91125df7e353fa2
tereshko-ivan <vanyakosmos84@gmail.com>
2024-02-01 21:26:42 +0300

    Update Main class add methods.

--- src/by/itstep/javalesson/task001/controller/Main.java
+++ src/by/itstep/javalesson/task001/controller/Main.java
@@ -1,9 +1,13 @@
 package by.itstep.javalesson.task001.controller;
 
 import by.itstep.javalesson.task001.model.logic.NumberLogic;
+import by.itstep.javalesson.task001.view.ConsolePrinter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(NumberLogic.sum(5,6));
+        ConsolePrinter.print(NumberLogic.sum(5,6) + "");
+        ConsolePrinter.print(NumberLogic.sub(6,5) + "");
+        ConsolePrinter.print(NumberLogic.mul(2,2) + "");
+        ConsolePrinter.print(NumberLogic.div(2,2) + "");
     }
 }
