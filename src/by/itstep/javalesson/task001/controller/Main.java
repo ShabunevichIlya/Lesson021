689b75f5e3c0daa2d164f9bd2adf2cf522d00329
tereshko-ivan <vanyakosmos84@gmail.com>
2024-02-01 21:17:22 +0300

    Add NumberLogic class with sum method.

--- src/by/itstep/javalesson/task001/controller/Main.java
+++ src/by/itstep/javalesson/task001/controller/Main.java
@@ -1,7 +1,9 @@
 package by.itstep.javalesson.task001.controller;
 
+import by.itstep.javalesson.task001.model.logic.NumberLogic;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing...");
+        System.out.println(NumberLogic.sum(5,6));
     }
 }
---
+++ src/by/itstep/javalesson/task001/model/logic/NumberLogic.java
@@ -0,0 +1,8 @@
+package by.itstep.javalesson.task001.model.logic;
+
+public class NumberLogic {
+
+    public static int sum (int a, int b) {
+        return a + b;
+    }
+}
